87f4ca5804e4790f5729ce289822a5f9b4a3bef1
PrateeshYadav <pratishyadavOP@gmail.com>
2019-08-17 09:35:37 +0530

    Create mario_less.c

---
+++ mario_less.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int userInput = get_int("ENTER a number : ");
+
+    for(int i = 0; i < userInput; i++)
+    {
+        for(int j = userInput; j > i; j--)
+        {
+            printf(" ");
+        }
+
+        for(int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+
+
+
+    }
+}
